c73853d93f9e4a49e45eaf8a36699f1ab81d68a7
Imanol Pinto <ipintoc@gmail.com>
2018-06-25 12:15:05 +0200

    Add env variables

--- src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/EnvControllerTest.java
@@ -1,15 +1,16 @@
 package test.pivotal.pal.tracker;
 
+import io.pivotal.pal.tracker.EnvController;
 import org.junit.Test;
 
 import java.util.Map;
-import io.pivotal.pal.tracker.EnvController;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnvControllerTest {
+
     @Test
-    public void getEnv() throws Exception {
+    public void getEnv() {
         EnvController controller = new EnvController(
             "8675",
             "12G",
